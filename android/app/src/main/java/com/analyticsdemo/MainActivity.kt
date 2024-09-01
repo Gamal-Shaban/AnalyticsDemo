fb6caad812ddea98c347a3f98926ac6dd0bcaa41
gamal@4tel.sa <>
2024-09-01 23:31:27 +0300

    add navigation

--- android/app/src/main/java/com/analyticsdemo/MainActivity.kt
+++ android/app/src/main/java/com/analyticsdemo/MainActivity.kt
@@ -1,10 +1,11 @@
+import android.os.Bundle
 package com.analyticsdemo
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
+import android.os.Bundle;
 class MainActivity : ReactActivity() {
 
   /**
@@ -12,7 +13,9 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "AnalyticsDemo"
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(null)
+    }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
